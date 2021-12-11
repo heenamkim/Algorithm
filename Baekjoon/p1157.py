61c7a60681bbe6f9036658c42e6225f11861ffe4
heenam kim <znzltiq111@naver.com>
2021-12-12 00:18:47 +0900

    baekjoon_1157

--- Baekjoon/p1157.py
+++ Baekjoon/p1157.py
@@ -2,14 +2,31 @@
 
 word = input()
 word = word.upper()
-alp = []
 
-for i in range(65,91):
-    i = str(i)
-    alp = alp.append(i)
-print(alp)
-'''
-for i in word:
-    for j in alp:
-        if i == j or i ==
-'''
+alp_list = []
+alp_count = []
+
+max_count = 0
+result = 0
+
+for upper_alp in range(65, 91):
+    alp_list.append(chr(upper_alp))
+    alp_count.append(0)
+
+for word_alp in word:
+    for alp_index, alp in enumerate(alp_list):
+        if word_alp == alp:
+            alp_count[alp_index] += 1
+
+max_count = max(alp_count)
+
+
+for count in alp_count:
+    if count == max_count:
+        result += 1
+
+if result >= 2 :
+    print('?')
+else:
+    print(alp_list[alp_count.index(max(alp_count))])
+
